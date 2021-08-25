b81710c49579099bb917485170b42882a4b19f98
LemonPapperSeasoning <tish386@aucklanduni.ac.nz>
2021-08-25 22:10:11 +1200

    Plot volumes

--- Task3/app.py
+++ Task3/app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
 import sqlite3 as sql
+import plotly
+import plotly.express as px
+import pandas as pd
+import json
 
 app = Flask(__name__)
+
+def connect_db():
+    connection = sql.connect("AlphaVantage.db")
+    cursor = connection.cursor()
+    return cursor
 
 @app.route('/')
 def root():
@@ -9,9 +18,7 @@ def root():
 
 @app.route('/GET/stocks')
 def get():
-    connection = sql.connect("AlphaVantage.db")
-    cursor = connection.cursor()
-    
+    connect_db()
     with connection:
         cursor.execute("""
                        SELECT DISTINCT stock FROM stock_weekly;
@@ -19,26 +26,28 @@ def get():
     available_stock = cursor.fetchall()
     return str(available_stock)
 
-# @app.route('/GET/stocks/{id}')
-# def get():
-#     connection = sql.connect("AlphaVantage.db")
-#     cursor = connection.cursor()
-    
-#     with connection:
-#         cursor.execute("""
-#                        SELECT DISTINCT stock FROM stock_weekly;
-#                        """)
-#     available_stock = cursor.fetchall()
-#     return str(available_stock)
 
 @app.route('/PLOT/stocks')
 def index():
-    connection = sql.connect("AlphaVantage.db")
-    cursor = connection.cursor()
-    
-    data = cursor.execute('SELECT * FROM stock_weekly').fetchall()
    
-    return render_template('index.html', name=str(data))
+    data = connect_db().execute('SELECT * FROM stock_weekly').fetchall()
+
+    return render_template('plot.html', name=str(data))
+
+
+@app.route('/plot/volume')
+def plot_volume():        
+    data = connect_db().execute(
+        'SELECT date,volume,stock FROM stock_weekly').fetchall()
+    
+    df = pd.DataFrame(data, columns=['year', 'volume', 'stock'])
+    
+    fig = px.line(df, x="year", y="volume", color='stock',
+                  title='Volume of Weekly trade')
+    
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    return render_template('plot.html', graphJSON=graphJSON)
 
 
 if __name__ == "__main__":
